82267935ff64362fba26a9f639e7a362c40e3a6c
BOCOVO <bocovojuste@gmail.com>
2022-12-06 23:18:49 +0100

    feat: open tab on preview button clicked

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let previewPanel: vscode.WebviewPanel | undefined = undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dbml-preview" is now active!');
+  vscode.commands.registerCommand("dbml-preview.openPreview", () => {
+    const columnToShowIn =
+      vscode.window.activeTextEditor?.viewColumn || vscode.ViewColumn.One;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dbml-preview.openPreview', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dbml-preview!');
-	});
+    if (previewPanel) {
+      previewPanel.reveal(columnToShowIn);
+    } else {
+      previewPanel = vscode.window.createWebviewPanel(
+        "data-base-schema-preview",
+        "Database Schema Preview",
+        columnToShowIn,
+        {}
+      );
 
-	context.subscriptions.push(disposable);
+      previewPanel.onDidDispose(
+        () => {
+          previewPanel = undefined;
+        },
+        null,
+        context.subscriptions
+      );
+    }
+  });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
